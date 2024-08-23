a2fb1c0333b05c19a3b4e7016287156dc6b46eb3
Gutto <guttolaudie@gmail.com>
2024-08-23 17:59:32 -0300

    test_5

--- src/app/main.py
+++ src/app/main.py
@@ -5,23 +5,39 @@ st.set_page_config(page_title="Previsão de Churn da Top Bank",
                    page_icon=":bank:",
                    layout="wide")
 
-PAGES = {
-    "Home": home.run,
-    "Explicação do Modelo": model_explain.run,
-    "Análise Exploratória de Dados": eda.run,
-    #"Top Clients": top_clients.run,
-    #"Previsão de Churn": predict.run,
-    #"Simulação de Ticket": ticket_simulation.run,
-    "Sobre": about.run
-}
+# Lists for page names and corresponding functions
+PAGE_NAMES = [
+    "Home",
+    "Explicação do Modelo",
+    "Análise Exploratória de Dados",
+    "Top Clients",
+    "Previsão de Churn",
+    "Simulação de Ticket",
+    "Sobre"
+]
+
+PAGE_FUNCTIONS = [
+    home.run,
+    model_explain.run,
+    eda.run,
+    top_clients.run,
+    predict.run,
+    ticket_simulation.run,
+    about.run
+]
 
 def main():
     # Add lateral bar
     st.sidebar.title("Navegação")
-    selection = st.sidebar.selectbox("Ir para", list(PAGES.keys()))
+    selection = st.sidebar.selectbox("Ir para", PAGE_NAMES)
 
-    page = PAGES[selection]
-    page()
+    # Find the index of the selected page
+    page_index = PAGE_NAMES.index(selection)
+
+    # Get the corresponding function from the PAGE_FUNCTIONS list
+    page_function = PAGE_FUNCTIONS[page_index]
+    page_function()
 
 if __name__ == "__main__":
     main()
+
